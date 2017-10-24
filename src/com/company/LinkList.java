5510d4b8aeff95a34f53ddf65aa0ad7878d9aff0
Andrei Kovalchuk <kovalchuk-spb@inbox.ru>
2017-10-24 22:24:50 +0300

    commit

--- src/com/company/LinkList.java
+++ src/com/company/LinkList.java
@@ -26,7 +26,7 @@ public class LinkList implements List, Stack, Queue {
         return 0;
     }
 
-        private Link deliteFirst() {
+    private Link deliteFirst() {
         Link temp = first;
         first = temp.next;
         return temp;
@@ -89,15 +89,14 @@ public class LinkList implements List, Stack, Queue {
         return newLink.data;
     }
 
-
     @Override
     public void add(Object obj) {
         Link newLink = new Link(obj);
-        Link current =first;
+        Link current = first;
         if (isEmpty())
             first = newLink;
         else {
-            while (current.next!=null){
+            while (current.next != null) {
                 current = current.next;
             }
             current.next = newLink;
@@ -106,25 +105,54 @@ public class LinkList implements List, Stack, Queue {
 
     @Override
     public Object poll() {
-        return null;
+        Link current = first;
+        if (isEmpty()) return null;
+        first = first.next;
+        return current;
     }
 
     @Override
     public Object get(int i) {
-        return null;
+        Link current = first;
+        int count = 0;
+        if (isEmpty())
+            return null;
+        else {
+            while (count++ != i) {
+                if (current.next == null)
+                    return null;
+                current = current.next;
+            }
+        }
+        return current;
     }
 
     @Override
     public Object remove(int i) {
-        return null;
+        Link current = first;
+        Link previous = first;
+        int count = 0;
+        if (isEmpty()) return null;
+        if (first.next == null) {
+            first = null;
+            return first;
+        }
+        while (count++ != i) {
+            if (current.next == null)
+                return null;
+            previous = current;
+            current = current.next;
+        }
+        previous.next = current.next;
+        return current;
     }
 
     @Override
     public int size() {
-        int count =0;
+        int count = 0;
         Link current = first;
-        if(isEmpty()) return  0;
-        while (current!=null){
+        if (isEmpty()) return 0;
+        while (current != null) {
             count++;
             current = current.next;
         }
@@ -133,12 +161,21 @@ public class LinkList implements List, Stack, Queue {
 
     @Override
     public void push(Object obj) {
-
+        Link newLink = new Link(obj);
+        if (isEmpty())
+            first = newLink;
+        else {
+            newLink.next = first;
+            first = newLink;
+        }
     }
 
     @Override
     public Object pop() {
-        return null;
+        Link current = first;
+        if (isEmpty()) return null;
+        first = first.next;
+        return current;
     }
 
     public static void main(String[] args) {
@@ -169,13 +206,32 @@ public class LinkList implements List, Stack, Queue {
             System.out.println("Удален элемент: " + linkDelFirst.data);
         }
         System.out.println("Список очищен!");
+//      очередь
         list.add("What");
         list.add("else");
         list.add("is");
         list.add("there");
         list.add("?");
-
         list.displayList();
+        System.out.println("Размер: " + list.size());
+        int size = list.size();
+        for (int j = 0; j < size; j++){
+            list.poll();
+            list.displayList();
+        }
+//        list.displayList();
+        list.push("ack");
+        list.push("St");
+        list.push("do");
+        list.push("to");
+        list.push("trying");
+        list.push("OK...");
+        list.displayList();
+        list.pop();
+        list.displayList();
+        list.remove(2);
+        list.displayList();
+        System.out.println(list.get(1));
     }
 
 }
